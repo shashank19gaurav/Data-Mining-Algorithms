7f1ed1e19371c4d8fc62d24375208502697ce00d
Shashank Gaurav <shashank19gaurav@gmail.com>
2016-04-13 08:25:24 +0530

    Fix Bugs, Attempt to generate Frequent Item Set

--- FP Growth/fpgrowth.py
+++ FP Growth/fpgrowth.py
@@ -19,6 +19,7 @@ class Node:
         
     def addChild(self, node):
         self.childs.append(node)
+        pass
 
     def insertTransaction(self, transactionString):
         # Two case - 1. Root Node , 2. Other Node
@@ -26,7 +27,7 @@ class Node:
         if len(transactionString) == 0:
             return
         if self.value == "Root":
-            print "Root Me Aaaya and Value hai", transactionString
+            # print "Root Me Aaaya and Value hai", transactionString
             # Check if any of the child has the same value as the first value of the transaction
             # If exists then increase its count and recursively call this function on the child node
             for child in self.childs:
@@ -46,14 +47,13 @@ class Node:
                 childNode.insertTransaction(transactionString[1:])
 
         else:
-            print "Root ke bete me Aaaya and Value hai", transactionString
+            # print "Root ke bete me Aaaya and Value hai", transactionString
             # The current node is not a root node
             # Check value accordingly
             if self.value == transactionString[:1] :
                 self.count +=1
                 # Check if further down, the string matches or not
                 print "Checking in child of ", self.value, "for ", transactionString
-                return
                 for child in self.childs:
                     if child.value == transactionString[1:2] :
                         # Found a child with the same value
@@ -75,6 +75,20 @@ class Node:
                 if len(transactionString) > 0:
                     childNode.insertTransaction(transactionString[1:])
 
+    def generateFrequentSet(self, item):
+        if self.value == item:
+            return self.value + " - " + str(self.count)
+
+        else:
+            frequentItemSet = []
+            for child in self.childs:
+                if item in child.generateFrequentSet(item):
+                    frequentItemSet.append(child.generateFrequentSet(item))
+
+            return frequentItemSet
+
+
+
 class Tree:
     def __init__(self):
         self.node = Node("Root")
@@ -82,14 +96,12 @@ class Tree:
     def updateWithTransaction(self, transactionString):
         self.node.insertTransaction(transactionString)
 
-
-tree = Tree()
-tree.updateWithTransaction("abc")
-
+    def findFrequentItemSetForItem(self, item):
+        print self.node.generateFrequentSet(item)
 
 def comparePriority(x, y):
     global countEachItem
-    return countEachItem[x] - countEachItem[y]
+    return countEachItem[y] - countEachItem[x]
 
 i = 0
 while i<nTransaction:
@@ -114,6 +126,18 @@ for transaction in transactions:
 
 print "Update transactions based on Priority :", transactionBasedOnPriority
 # We have transaction based on the priority
-# Next task is to create FP Tree
+# Next task is to create FP Tree 
 
+tree = Tree()
+
+for transaction in transactionBasedOnPriority:
+    tree.updateWithTransaction(transaction)
+
+# Tree generated
+# No go till the end of the tree
+# List all the transactions and finally merge them back together
+# 
+
+for item in eachItem:
+    tree.findFrequentItemSetForItem(item)
 
